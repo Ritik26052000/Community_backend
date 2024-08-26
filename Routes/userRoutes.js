0bf7bd130b7617881f327e6bbb5ce7db0366af7d
Ritik26052000 <sonwanianshu2000@gmail.com>
2024-08-26 11:16:12 +0530

    new deploy

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -7,7 +7,6 @@ const registerModel = require("../models/registerModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const blackListModel = require("../models/blackListModel");
-
 const userRouter = express.Router();
 
 const storage = multer.diskStorage({
